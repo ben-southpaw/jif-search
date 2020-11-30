e9bcbbbb78ba0f6e6dfeb1423ae542e48e2c2f1b
Benjamin Dryden <benjamin@adventureclub.io>
2020-11-30 11:19:00 +0100

    Updated with stories removed

--- src/App.svelte
+++ src/App.svelte
@@ -1,6 +1,7 @@
 <script>
 	let search;
 	let loading = false;
+	$: bgImage;
 	const API_URL = `https://api.giphy.com/v1/gifs/search?api_key=${API_KEY}&rating=pg&q=`;
 
 	let gifs = [];
@@ -13,43 +14,71 @@
 		const json = await response.json();
 		gifs = json.data.map(gif => gif.images.fixed_height.url);
 		loading = false;
+		bgImage = `style=background-image: url('${gifs[0]}')}`
 	}
+
+
+
+
+
+	let index = Math.floor(Math.random() * gifs.length);
 
 </script>
 
-<form action="" on:submit|preventDefault={formSubmitted}>
-	<label for="search">Search</label>
-	<input bind:value={search} id="search" name="search" />
-	<button type="submit">Go</button>
-</form>
+<div class="site-wrapper" {{bgImage}}>
+	<div class="site-wrapper-inner">
+		<div class="container">
+			<div class="masthead clearfix">
+				<div class="container inner">
+					<h3 class="masthead-brand">JIFS</h3>
+				</div>
+			</div>
+			<div class="inner cover">
+				<form class="form-group" action="" on:submit|preventDefault={formSubmitted}>
+					<label class="form-check-label" for="search">{search}</label>
+					<input class="form-control" bind:value={search} id="search" name="search" />
+					<div class="buttonContainer">
+						<button class="btn btn-primary" type="submit">Search</button>
+						<button class="btn btn-primary" type="submit">Shuffle</button>
+					</div>
+				</form>
+			</div>
+		</div>
+	</div>
+
+</div>
+
 {#if loading}
 	<img src="https://media2.giphy.com/media/l3nWhI38IWDofyDrW/giphy.gif?cid=790b76115d055ab7424f75514dcb4d7a&rid=giphy.gif" alt="loading" >
 {/if}
 
 <div class="results">
 	{#each gifs as gif}
-		<img alt="jifs" src={gif}>
+		<img alt="jifs" src={gifs[0]}>
 	{/each}
 </div>
 
 
 <style lang="scss">
-
 	form {
 		text-align: center;
 		margin: 5%;
 		label {
 			display: block;
 		}
-		button {
-			display: block;
-			width: 5em;
+		input {
+			width: 33%;
 			margin: auto;
-			margin-top: 1em;
+		}
+		.buttonContainer {
+			display: inline-flex;
+			button {
+				display: block;
+				width: 5em;
+				margin: 1em 1em auto;
+			}
 		}
 	}
-
-
 	.results {
 		column-count: 3;
 		img {
@@ -59,10 +88,102 @@
 	}
 
 
+	/*
+ * Globals
+ */
+	/* Links */
+	a,
+	a:focus,
+	a:hover {
+		color: #fff;
+	}
 
-	//@media (min-width: 640px) {
-	//	main {
-	//		max-width: none;
-	//	}
-	//}
+	/* Custom default button */
+	.btn-default,
+	.btn-default:hover,
+	.btn-default:focus {
+		color: #333;
+		text-shadow: none; /* Prevent inheritence from `body` */
+		background-color: #fff;
+		border: 1px solid #fff;
+	}
+	/*
+     * Base structure
+     */
+	html,
+	body {
+		height: 100%;
+		background-color: #fff;
+	}
+	body {
+		color: #fff;
+		text-align: center;
+		text-shadow: 0 1px 3px rgba(0,0,0,.5);
+	}
+	/* Extra markup and styles for table-esque vertical and horizontal centering */
+	.site-wrapper {
+		display: table;
+		width: 100%;
+		height: 600px;
+		//background-color: #333;
+		/*height: 100%; */
+		/*min-height: 100%;*/
+	}
+	.site-wrapper-inner {
+		display: table-cell;
+		vertical-align: top;
+	}
+	.cover-container {
+		margin-right: auto;
+		margin-left: auto;
+	}
+
+	/* Padding for spacing */
+	.inner {
+		padding: 30px;
+		text-align: -webkit-center;
+		text-align: center;
+	}
+
+	@media (min-width: 768px) {
+		.masthead-brand {
+			float: left;
+		}
+		.masthead-nav {
+			float: right;
+		}
+	}
+
+
+	/*
+     * Cover
+     */
+
+	.cover {
+		padding: 0 20px;
+	}
+	.cover .btn-lg {
+		padding: 10px 20px;
+		font-weight: bold;
+	}
+
+	/*
+     * Affix and center
+     */
+
+	@media (min-width: 768px) {
+		/* Pull out the header and footer */
+		.masthead {
+			position: fixed;
+			top: 0;
+		}
+		/* Start the vertical centering */
+		.site-wrapper-inner {
+			vertical-align: middle;
+		}
+
+	}
+
+
+
 </style>
